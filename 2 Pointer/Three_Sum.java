38e67ccffa3291995d5a0358392420433e637849
Tanushi Agnihotri <tanushiagni@gmail.com>
2026-02-01 14:22:47 +0530

    three sum done

---
+++ 2 Pointer/Three_Sum.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Three_Sum {
+        public List<List<Integer>> threeSum(int[] nums) {
+
+            Arrays.sort(nums);
+
+            List<List<Integer>> res = new ArrayList<>();
+
+            for(int f=0; f<nums.length; f++){
+
+                if(nums[f]>0){
+                    break;
+                }
+                if(f==0 ||nums[f]!=nums[f-1]){
+                    twoSumHelper(f,nums,res);
+                }
+            }
+            return res;
+        }
+
+        public void twoSumHelper(int f,int[] nums, List<List<Integer>> res){
+
+            int i=f+1, j = nums.length-1;
+
+            while(i<j){
+
+                int sum = nums[f] + nums[i] + nums[j];
+
+                if(sum>0){
+                    j--;
+                }
+
+                else if (sum<0){
+                    i++;
+                }
+
+                else{
+                    res.add(Arrays.asList(nums[f],nums[i],nums[j]));
+                    i++;
+                    j--;
+
+                    while(i<j && nums[i]==nums[i-1]){
+                        i++;
+                    }
+
+                    while(i<j && nums[j]==nums[j+1]){
+                        j--;
+                    }
+                }
+            }
+        }
+    }
+
