b3b2b73219f47e09d1ca5f3b155228725a8aae94
Tanushi Agnihotri <tanushiagni@gmail.com>
2026-02-01 17:38:53 +0530

    Sort colors done

---
+++ 2 Pointer/Sort_Colors.java
@@ -0,0 +1,31 @@
+public class Sort_Colors {
+        public void sortColors(int[] nums) {
+
+            int i =0, k=0, j=nums.length-1;
+
+            while(k<=j){
+
+                if(nums[k]==1){
+                    k++;
+                }
+
+                else if (nums[k]==2){
+                    int temp = nums[k];
+                    nums[k] = nums[j];
+                    nums[j] = temp;
+                    j--;
+                }
+
+                else{
+                    int temp = nums[k];
+                    nums[k] = nums[i];
+                    nums[i] = temp;
+
+                    i++;
+                    k++;
+                }
+            }
+
+        }
+    }
+
